bafa1ebf288f557cb36d4a1a0eebf3f63c6392af
Thom Volker <t.b.volker@uu.nl>
2020-07-09 21:56:51 +0200

    Add population election proportions to the polls

--- 1.Data_Handling.R
+++ 1.Data_Handling.R
@@ -1,4 +1,75 @@
+#########################################################################################
+## File for all data handling                                                          ##
+#########################################################################################
 
-data <- read.csv("president_general_polls_2016_ed.csv")[,-(27:31)]
+## Load tidyverse for the pipe, and the dplyr functions
+library(tidyverse)
+
+## Load in the data (due to a semicolon after each row, we get rows containing only a
+## semicolon, which we exclude from the data, additionally, there are five columns of
+## nonsense in the dataframe (probably due to how I read the data in), which are 
+## excluded as well).
+data <- read.csv("president_general_polls_2016_ed.csv")[,-(27:31)] %>% filter(cycle != ";")
+
+## Look at all unique states
+unique(data$state)
+
+## Load in a dataset with the population election results in all states
+pop_dat <- read.csv("~/Downloads/1976-2016-president.csv")
+
+## In the population data, extract only the rows concerning 2016, remove the variable
+## party, which is not of any interest, but will mess up the pivoting, keep only Trump
+## and Clinton, extract all variables, but sum the votes for the candidates based on 
+## the state (this happens because in New York, there are three rows with Clinton data,
+## all indicating a different party, and we need to add these in a single value). 
+## Then, using pivot_wider make separate columns for trump and clinton votes, instead
+## of separate rows, and compute the proportion of votes for trump and clinton
+pop_dat <- pop_dat %>% 
+  filter(year == 2016) %>%
+  select(-party) %>%
+  filter(candidate == "Trump, Donald J." | candidate == "Clinton, Hillary") %>%
+  mutate(candidate = recode(candidate, 
+                            "Trump, Donald J." = "Trump", 
+                            "Clinton, Hillary" = "Clinton")) %>%
+  group_by(state, candidate) %>%
+  summarise(year = unique(year),
+            state = unique(state),
+            state_po = unique(state_po),
+            state_fips = unique(state_fips), 
+            state_cen = unique(state_cen),
+            state_ic = unique(state_ic),
+            office = unique(office),
+            candidate = unique(candidate),
+            writein = unique(writein),
+            candidatevotes = sum(candidatevotes),
+            totalvotes = unique(totalvotes),
+            version = unique(version),
+            notes = unique(notes)) %>%
+  pivot_wider(names_from = candidate, values_from = candidatevotes) %>%
+  mutate(prop_clinton = Clinton / totalvotes,
+         prop_trump = Trump / totalvotes)
+
+## Now extract only the states and the votes
+pop_votes <- pop_dat[,c("state", "prop_clinton", "prop_trump")]
+
+## Add the district data manually, because it is not included in the data thus far, and
+## add the data of the U.S. data as a whole
+pop_votes <- bind_rows(pop_votes, 
+                       data.frame(state = c("U.S.", "Maine CD-1", "Maine CD-2", 
+                                            "Nebraska CD-1", "Nebraska CD-2", "Nebraska CD-3"),
+                                  prop_clinton = c(.482, .5396, .4097, .36, .45, .20),
+                                  prop_trump = c(.461, .3915, .5126, .56, .47, .74)))
+
+## Now add variables to the total dataset for the population score of trump and clinton
+## and loop over all states to add the population score in that state to the dataset
+## containing the election info
+data$pop_clinton <- data$pop_trump <- NA
+
+for (i in 1:nrow(pop_votes)) {
+  data$pop_clinton[data$state %in% pop_votes$state[i]] <- pop_votes$prop_clinton[i]
+  data$pop_trump[data$state %in% pop_votes$state[i]] <- pop_votes$prop_trump[i]
+}
+
+
 
 
