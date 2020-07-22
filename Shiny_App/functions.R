1cea4c0ae9102edfd93d8636470a0f91688d53e6
Thom Volker <t.b.volker@uu.nl>
2020-07-22 17:19:49 +0200

    Added choice of IV

--- Shiny_App/functions.R
+++ Shiny_App/functions.R
@@ -16,57 +16,47 @@ plot_results <- function(data, who) {
   us_trump <- 46.1
   us_clinton <- 48.2
   
+  base_plot <- data %>% 
+    ggplot(aes(x = xvar)) + 
+    ylim(0,100) +
+    ylab("Proportion of votes") +
+    scale_color_brewer(palette = "Set1") +
+    theme_classic()
+    
+  
   if (who == "Trump") {
     
     state_intercept <- unique(data$pop_trump)
     
-    data %>%
-      ggplot() +
-      geom_point(mapping = aes(x = enddate, y = rawpoll_trump, color = "Raw Score Trump"), size = .5) +
-      geom_point(mapping = aes(x = enddate, y = adjpoll_trump, color = "Adjusted Score Trump"), size = .5) +
+    base_plot + 
+      geom_point(mapping = aes(y = rawpoll_trump, color = "Raw Score Trump"), size = .5) +
+      geom_point(mapping = aes(y = adjpoll_trump, color = "Adjusted Score Trump"), size = .5) +
       geom_abline(intercept = us_trump, slope = 0, show.legend = TRUE) +
-      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE) +
-      scale_color_brewer(palette = "Set1") +
-      ylim(0, 100) +
-      ylab("Proportion of votes") +
-      ggtitle(paste0("Votes for Trump")) +
-      theme_classic()
+      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE)
   }
   
   else if (who == "Clinton") {
     
     state_intercept <- unique(data$pop_clinton)
     
-    data %>%
-      ggplot() +
-      geom_point(mapping = aes(x = enddate, y = rawpoll_clinton, color = "Raw Score Clinton"), size = .5, shape = 2) +
-      geom_point(mapping = aes(x = enddate, y = adjpoll_clinton, color = "Adjusted Score Clinton"), size = .5, shape = 2) +
+    base_plot + 
+      geom_point(mapping = aes(y = rawpoll_clinton, color = "Raw Score Clinton"), size = .5, shape = 2) +
+      geom_point(mapping = aes(y = adjpoll_clinton, color = "Adjusted Score Clinton"), size = .5, shape = 2) +
       geom_abline(intercept = us_clinton, slope = 0, show.legend = TRUE) +
-      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE) +
-      scale_color_brewer(palette = "Set1") +
-      ylim(0, 100) +
-      ylab("Proportion of votes") +
-      ggtitle(paste0("Votes for Clinton")) +
-      theme_classic()
+      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE)
   }
   
   else if (who == "Trump & Clinton") {
     
     state_intercept <- unique(data$pop_clinton)
     
-    data %>%
-      ggplot() +
-      geom_point(mapping = aes(x = enddate, y = rawpoll_trump, color = "Raw Score Trump"), size = .5) +
-      geom_point(mapping = aes(x = enddate, y = adjpoll_trump, color = "Adjusted Score Trump"), size = .5) +
-      geom_point(mapping = aes(x = enddate, y = rawpoll_clinton, color = "Raw Score Clinton"), size = .5, shape = 2) +
-      geom_point(mapping = aes(x = enddate, y = adjpoll_clinton, color = "Adjusted Score Clinton"), size = .5, shape = 2) +
+    base_plot +
+      geom_point(mapping = aes(y = rawpoll_trump, color = "Raw Score Trump"), size = .5) +
+      geom_point(mapping = aes(y = adjpoll_trump, color = "Adjusted Score Trump"), size = .5) +
+      geom_point(mapping = aes(y = rawpoll_clinton, color = "Raw Score Clinton"), size = .5, shape = 2) +
+      geom_point(mapping = aes(y = adjpoll_clinton, color = "Adjusted Score Clinton"), size = .5, shape = 2) +
       geom_abline(intercept = us_clinton, slope = 0, show.legend = TRUE) +
-      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE) +
-      scale_color_brewer(palette = "Set1") +
-      ylim(0, 100) +
-      ylab("Proportion of votes") +
-      ggtitle(paste0("Votes for Clinton")) +
-      theme_classic()
+      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE)
     
   }
 }
--- Shiny_App/shinyapp.R
+++ Shiny_App/shinyapp.R
@@ -1,5 +1,6 @@
 
 source("~/Documents/M_S/SA_Peter/Survey_Data_Analysis/1.Data_Handling.R")
+source("~/Documents/M_S/SA_Peter/Survey_Data_Analysis/Shiny_App/functions.R")
 
 ui <- fluidPage(
   fluidRow(
@@ -15,37 +16,25 @@ ui <- fluidPage(
            )
   ),
   fluidRow(
+    column(3,
+           selectInput("xvar", "Independent variable", 
+                       c("forecastdate", "startdate", "enddate",
+                         "grade", "samplesize", "population"))
+           )
+  ),
+  fluidRow(
     plotOutput("results")
   )
 )
-
-plot_results <- function(data, who) {
-  
-  us_intercept <- 46.1
-  
-  if (who == "Trump") {
-    
-    state_intercept <- unique(data$pop_trump)
-    
-    data %>%
-      ggplot() +
-      geom_point(mapping = aes(x = enddate, y = rawpoll_trump, color = "Raw Score Trump"), size = .5) +
-      geom_point(mapping = aes(x = enddate, y = adjpoll_trump, color = "Adjusted Score Trump"), size = .5) +
-      geom_abline(intercept = us_intercept, slope = 0, show.legend = TRUE) +
-      geom_abline(intercept = state_intercept, slope = 0, linetype = "dashed", show.legend = TRUE) +
-      scale_color_brewer(palette = "Set1") +
-      ylim(0, 100) +
-      ylab("Proportion of votes") +
-      ggtitle(paste0("Votes for Trump")) +
-      theme_classic()
-  }
-}
 
 server <- function(input, output, session) {
   
   dataset <- reactive(data %>% 
                         filter(state == input$state) %>%
-                        filter(enddate >= input$period[1] & enddate <= input$period[2]))
+                        filter(enddate >= input$period[1] & enddate <= input$period[2]) %>%
+                        select(xvar = input$xvar, state, rawpoll_clinton, rawpoll_trump, 
+                               adjpoll_clinton, adjpoll_trump, pop_clinton, pop_trump,
+                               raw_adj_dif_clinton, raw_adj_dif_trump))
   
   output$results <- renderPlot({
     dataset() %>% plot_results(., input$who)
