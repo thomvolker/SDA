cb8adba4fcedda602d542ea238849ccf26652719
Thom Volker <t.b.volker@uu.nl>
2020-08-25 15:44:00 +0200

    Adjusted text labels date and grade.

--- shinyelectionbias/functions.R
+++ shinyelectionbias/functions.R
@@ -26,6 +26,15 @@ plot_results <- function(data, who, xvar, state, box_labs) {
           legend.title = element_blank(),
           axis.title.x = element_text(vjust = -2))
   
+  mean_est <- data %>%
+    group_by(raw_adj) %>%
+    summarise(mean_trump = mean(trump, na.rm = TRUE),
+              mean_clinton = mean(clinton, na.rm = TRUE),
+              median_trump = median(trump, na.rm = TRUE),
+              median_clinton = median(clinton, na.rm = TRUE),
+              median_dif = median(trump - clinton, na.rm = TRUE))
+    
+  
   if (xvar == "enddate") {
     
     monthly_means <- data %>%
@@ -49,17 +58,26 @@ plot_results <- function(data, who, xvar, state, box_labs) {
                         intercept = state_trump, 
                         slope = 0)) +
         scale_linetype_manual(values = "dashed") +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_trump,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
         annotate("label", 
                  x = min(data$months), 
-                 y = max(data$trump, na.rm = T) + 5, 
+                 y = max(c(data$trump, state_trump), na.rm = T) + (max(data$trump, na.rm = T) - min(data$trump, na.rm = T))/5, 
                  label = paste0("Election ", state, ": ", round(state_trump, digits = 2), "%"), 
                  hjust = 0) +
-        geom_label(data = monthly_means,
-                   aes(x = months, 
-                       y = rep(c(5,12.5), length(unique(months))),
-                       label = paste0(round(month_mean_trump, digits = 1), "%")),
-                   position = "identity", 
-                   show.legend = FALSE, 
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(min(data$months), 2),
+                                 y = c(min(data$trump, na.rm = T) - 1,
+                                       min(data$trump, na.rm = T) - 
+                                         (max(c(data$trump, state_trump), na.rm = T) - 
+                                            min(c(data$trump, state_trump), na.rm = T))/10 - 1),
+                                 label = paste0(raw_adj, " mean: ", round(mean_trump, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
                    hjust = 0)
       
     }
@@ -71,18 +89,28 @@ plot_results <- function(data, who, xvar, state, box_labs) {
         geom_abline(aes(linetype = paste0("True population value in ", state), 
                         intercept = state_clinton, 
                         slope = 0)) +
-        scale_linetype_manual(values = "dashed") +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_clinton,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
+        scale_linetype_manual(values = c("dashed", "dashed", "dashed")) +
         annotate("label", 
                  x = min(data$months), 
-                 y = max(data$clinton, na.rm = T) + 5, 
+                 y = max(c(data$clinton, state_clinton), na.rm = T) + 
+                       (max(data$clinton, na.rm = T) - min(data$clinton, na.rm = T))/5, 
                  label = paste0("Election ", state, ": ", round(state_clinton, digits = 2), "%"), 
                  hjust = 0) +
-        geom_label(data = monthly_means,
-                   aes(x = months, 
-                       y = rep(c(5,12.5), length(unique(months))),
-                       label = paste0(round(month_mean_clinton, digits = 1), "%")),
-                   position = "identity", 
-                   show.legend = FALSE, 
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(min(data$months), 2),
+                                 y = c(min(data$clinton, na.rm = T) - 1,
+                                       min(data$clinton, na.rm = T) - 
+                                         (max(c(data$clinton, state_clinton), na.rm = T) - 
+                                            min(c(data$clinton, state_clinton), na.rm = T))/10 - 1),
+                                 label = paste0(raw_adj, " mean: ", round(mean_clinton, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
                    hjust = 0)
       
     }
@@ -92,19 +120,31 @@ plot_results <- function(data, who, xvar, state, box_labs) {
         geom_point(aes(y = trump - clinton), 
                    size = .5) +
         geom_abline(aes(linetype = paste0("True population value in ", state), 
-                        intercept = state_trump - state_clinton, slope = 0)) +
-        scale_linetype_manual(values = "dashed") +
+                        intercept = state_trump - state_clinton, 
+                        slope = 0)) +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_trump - mean_clinton,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
+        scale_linetype_manual(values = c("dashed", "dashed", "dashed")) +
         annotate("label", 
                  x = min(data$months), 
-                 y = max(data$trump - data$clinton, na.rm = T) + 5, 
+                 y = max(c(data$trump - data$clinton, state_trump - state_clinton), na.rm = T) + 
+                       (max(data$trump - data$clinton, na.rm = T) - 
+                          min(data$trump - data$clinton, na.rm = T))/5, 
                  label = paste0("Election ", state, ": ", round(state_trump - state_clinton, digits = 2), "%"), 
                  hjust = 0) +
-        geom_label(data = monthly_means,
-                   aes(x = months, 
-                       y = rep(c(-75, -90), length(unique(months))),
-                       label = paste0(round(month_mean_trump - month_mean_clinton, digits = 1), "%")),
-                   position = "identity", 
-                   show.legend = FALSE, 
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(min(data$months), 2),
+                                 y = c(min(data$trump - data$clinton, na.rm = T) - 1,
+                                       min(data$trump - data$clinton, na.rm = T) - 
+                                         (max(c(data$trump - data$clinton, state_trump - state_clinton), na.rm = T) - 
+                                            min(c(data$trump - data$clinton, state_trump - state_clinton), na.rm = T))/10 - 1),
+                                 label = paste0(raw_adj, " mean: ", round(mean_trump - mean_clinton, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
                    hjust = 0)
     }
   }
@@ -114,17 +154,24 @@ plot_results <- function(data, who, xvar, state, box_labs) {
     text_medians <- data %>%
       group_by(xvar, raw_adj) %>%
       summarise(trump_m = median(trump),
-                clinton_m = median(clinton))
+                clinton_m = median(clinton),
+                dif_m = median(trump - clinton))
     
     if (who == "trump") {
       gg_box_plot <- base_plot +
         geom_abline(aes(linetype = paste0("True population value in ", state), 
                         intercept = state_trump, 
-                        slope = 0)) +
-        scale_linetype_manual(values = "dashed") +
+                        slope = 0),
+                    linetype = "dashed") +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_trump,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
         annotate("label", 
                  x = unique(sort(data$xvar))[1], 
-                 y = max(data$trump, na.rm = T), 
+                 y = max(data$trump, na.rm = T) + 2, 
                  label = paste0("Election ", state, ": ", round(state_trump, digits = 2), "%"), 
                  hjust = 0, 
                  vjust = 0) +
@@ -132,7 +179,17 @@ plot_results <- function(data, who, xvar, state, box_labs) {
                                    fill = raw_adj, 
                                    color = NULL),
                      outlier.alpha = .25,
-                     position = "dodge2")
+                     position = "dodge2") +
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(unique(sort(data$xvar))[1], 2),
+                                 y = c(min(data$trump, na.rm = T) - 2,
+                                       min(data$trump, na.rm = T) - 
+                                         (max(c(data$trump, state_trump), na.rm = T) - 
+                                            min(c(data$trump, state_trump), na.rm = T))/10 - 2),
+                                 label = paste0(raw_adj, " mean: ", round(mean_trump, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
+                   hjust = 0)
       
       if(box_labs) {
         gg_box_plot + 
@@ -154,18 +211,35 @@ plot_results <- function(data, who, xvar, state, box_labs) {
       gg_box_plot <- base_plot +
         geom_abline(aes(linetype = paste0("True population value in ", state), 
                         intercept = state_clinton, 
-                        slope = 0)) +
-        scale_linetype_manual(values = "dashed") +
+                        slope = 0),
+                    linetype = "dashed") +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_clinton,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
         annotate("label", 
                  x = unique(sort(data$xvar))[1], 
-                 y = max(data$clinton, na.rm = T), 
+                 y = max(data$clinton, na.rm = T) + 2, 
                  label = paste0("Election ", state, ": ", round(state_clinton, digits = 2), "%"), 
                  hjust = 0, 
                  vjust = 0) +
         geom_boxplot(mapping = aes(y = clinton, 
                                    fill = raw_adj, 
                                    color = NULL),
-                     outlier.alpha = .25)
+                     outlier.alpha = .25,
+                     position = "dodge2") +
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(unique(sort(data$xvar))[1], 2),
+                                 y = c(min(data$clinton, na.rm = T) - 2,
+                                       min(data$clinton, na.rm = T) - 
+                                         (max(c(data$clinton, state_clinton), na.rm = T) - 
+                                            min(data$clinton, na.rm = T))/10 - 2),
+                                 label = paste0(raw_adj, " mean: ", round(mean_clinton, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
+                   hjust = 0)
       
       if(box_labs) {
         gg_box_plot + 
@@ -186,26 +260,43 @@ plot_results <- function(data, who, xvar, state, box_labs) {
       gg_box_plot <- base_plot +
         geom_abline(aes(linetype = paste0("True population value in ", state), 
                         intercept = state_trump - state_clinton, 
-                        slope = 0)) +
-        scale_linetype_manual(values = "dashed") +
+                        slope = 0),
+                    linetype = "dashed") +
+        geom_abline(data = mean_est,
+                    mapping = aes(color = raw_adj,
+                                  intercept = mean_trump - mean_clinton,
+                                  slope = c(0,0)),
+                    linetype = "dashed",
+                    show.legend = FALSE) +
         annotate("label", 
                  x = unique(sort(data$xvar))[1], 
-                 y = max(data$trump - data$clinton, na.rm = T), 
+                 y = max(data$trump - data$clinton, na.rm = T) + 2, 
                  label = paste0("Election ", state, ": ", round(state_trump - state_clinton, digits = 2), "%"), 
                  hjust = 0, 
                  vjust = 0) +
         geom_boxplot(mapping = aes(y = trump - clinton, 
                                    fill = raw_adj, 
                                    color = NULL),
-                     outlier.alpha = .25)
+                     outlier.alpha = .25,
+                     position = "dodge2") +
+        geom_label(data = mean_est,
+                   mapping = aes(x = rep(unique(sort(data$xvar))[1], 2),
+                                 y = c(min(data$trump - data$clinton, na.rm = T) - 1,
+                                       min(data$trump - data$clinton, na.rm = T) - 
+                                         (max(c(data$trump - data$clinton, state_trump - state_clinton), na.rm = T) - 
+                                            min(c(data$trump - data$clinton, state_trump - state_clinton), na.rm = T))/10 - 1),
+                                 label = paste0(raw_adj, " mean: ", round(mean_trump - mean_clinton, 2), "%")),
+                   position = "identity",
+                   show.legend = FALSE,
+                   hjust = 0)
       
       if(box_labs) {
         gg_box_plot + 
           geom_label(data = text_medians,
-                     mapping = aes(y = trump_m - clinton_m,
+                     mapping = aes(y = dif_m,
                                    group = xvar,
                                    fill = raw_adj,
-                                   label = paste0(round(trump_m - clinton_m, digits = 0)),
+                                   label = paste0(round(dif_m, digits = 0)),
                                    vjust = 1.2),
                      position = position_dodge2(width = 0.75),
                      colour = "white",
